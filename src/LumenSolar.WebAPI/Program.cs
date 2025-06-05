9472b061b2838134469f68b437a92fd57322b53a
Gabriel dos Santos Lemos <gsl@vimansca.com.br>
2025-06-05 10:20:41 -0300

    Altera appsettings.json.

--- src/LumenSolar.WebAPI/Program.cs
+++ src/LumenSolar.WebAPI/Program.cs
@@ -112,9 +112,10 @@ app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
